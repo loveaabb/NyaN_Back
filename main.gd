3c410aea44d2c2a1444a1224d273e299d45cb7c5
Loveaabb <loveaabbcraft@163.com>
2024-09-08 23:26:52 +0800

    change title

--- main.gd
+++ main.gd
@@ -18,7 +18,7 @@ var RNG = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	reset_game()
-
+	get_window().title = "NyaN-Back by Loveaabb"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
